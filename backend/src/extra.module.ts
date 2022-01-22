4dcce75a940dc5a04b599d933087a1d1e0a0c1e6
Apa17 <mati.poli13@gmail.com>
2022-01-22 01:45:20 -0300

    rest api hecha con nestjs y typeorm funcional

--- backend/src/extra.module.ts
+++ backend/src/extra.module.ts
@@ -9,11 +9,14 @@ import { User } from "src/entities/user.entity";
 import { FolderService } from "src/services/folder.service";
 import { ItemService } from "src/services/item.service";
 import { UserService } from "src/services/user.service";
+import { PersonaController } from "./controllers/persona.controller";
+import { Persona } from "./entities/persona.entity";
+import { PersonaService } from "./services/persona.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Item, Folder, User])],
-    providers: [ItemService, FolderService, UserService],
-    controllers: [ItemController, FolderController, UserController]
+    imports: [TypeOrmModule.forFeature([Persona,Item, Folder, User])],
+    providers: [PersonaService, ItemService, FolderService, UserService],
+    controllers: [PersonaController, ItemController, FolderController, UserController]
 })
 export class ExtraModule{
 
